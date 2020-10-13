018cdb24c2dc14ba21527870f115776317018be3
YLCHUN <youlianchunios@163.com>
2020-10-13 22:20:33 +0800

    review

---
+++ LinkMap/HorizontalScrollView.h
@@ -0,0 +1,16 @@
+//
+//  HorizontalScrollView.h
+//  LinkMap
+//
+//  Created by YLCHUN on 2020/10/13.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HorizontalScrollView : NSScrollView
+
+@end
+
+NS_ASSUME_NONNULL_END
